87397746620c61a273d9946c60f0c59896d34ccc
ObiFlo1 <rubflores1000@gmail.com>
2024-04-08 13:32:34 -0500

    went ahead and did the non function stuff with contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,27 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 function ContactPage() {
   return (
-    <div>
-      <h1>ContactPage</h1>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form className={styles.form}>
+          <input />
+          <input />
+          <input />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   );
 }
